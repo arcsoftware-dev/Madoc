01a21a39562b5ece7b5987632bd9230ba9b29a0e
Jake <arcdev.tech@gmail.com>
2025-09-04 10:46:14 -0400

    Update news articles

--- src/main/java/dev/arcsoftware/madoc/repository/NewsRepository.java
+++ src/main/java/dev/arcsoftware/madoc/repository/NewsRepository.java
@@ -16,22 +16,38 @@ public class NewsRepository {
          article1.setContent("Madoc is a new platform for managing your gaming news and standings.");
          article1.setSummary("Madoc is a new platform for managing your gaming news and standings.");
          article1.setAuthor("Admin");
-         article1.setDate(LocalDateTime.of(2025, 9, 21, 12, 59, 0));
+         article1.setDate(LocalDateTime.of(2025, 8, 1, 14, 55, 0));
 
          NewsArticleDto article2 = new NewsArticleDto();
          article2.setTitle("League Fees 2025");
          article2.setContent("""
-                 League fees for the 2025 season are now due. Please ensure you pay your fees by the end of the month to avoid penalties.
-                 The deposited amount for the season is $100.
-                 The total amount for the season is $400.  This must be paid in full by the end of September 2025.
+                 League fees for the 2025 season are now due. Please ensure you pay your fees by the August 15th.
+                 The deposit amount for the season is $100.
+                 The total amount for the season is $515.  Full payment date will be provided closer to the beginning of the season.
                  """);
          article2.setSummary("""
-                 League fees for the 2025 season are now due. Please ensure you pay your fees by the end of the month to avoid penalties.
-                 The deposited amount for the season is $100.
-                 The total amount for the season is $400.  This must be paid in full by the end of September 2025.
+                 League fees for the 2025 season are now due. Please ensure you pay your fees by the August 15th.
+                 The deposit amount for the season is $100.
+                 The total amount for the season is $515.  Full payment date will be provided closer to the beginning of the season.
                  """);
          article2.setAuthor("Admin");
-         article2.setDate(LocalDateTime.of(2025, 6, 23, 15, 23, 45));
-         return List.of(article2, article1);
+         article2.setDate(LocalDateTime.of(2025, 8, 11, 19, 1, 0));
+
+         NewsArticleDto article3 = new NewsArticleDto();
+         article3.setTitle("2025/2026 Season Information");
+         article3.setContent("""
+                 The returning player draft will be held on September 3rd, 2025 at 7PM at Oscars.
+                 The 2025/2026 season will begin the week of September 14th 2025, with all games being played at Century Gardens.
+                 Rosters and schedules will be finalized and send out the week of September 8th.
+                 """);
+         article3.setSummary("""
+                 The returning player draft will be held on September 3rd, 2025 at 7PM at Oscars.
+                 The 2025/2026 season will begin the week of September 14th 2025, with all games being played at Century Gardens.
+                 Rosters and schedules will be finalized and sent out the week of September 8th.
+                 """);
+         article3.setAuthor("Admin");
+         article3.setDate(LocalDateTime.of(2025, 8, 20, 13, 36, 0));
+
+         return List.of(article3, article2, article1);
      }
 }
