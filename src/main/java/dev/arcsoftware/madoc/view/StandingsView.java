146ee4be85c1d31b8277b590f02e63f7f6762207
Jake <arcdev.tech@gmail.com>
2025-10-30 13:25:39 -0400

    Add year dropdown to standings page

--- src/main/java/dev/arcsoftware/madoc/view/StandingsView.java
+++ src/main/java/dev/arcsoftware/madoc/view/StandingsView.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,6 +31,11 @@ public class StandingsView {
                          SeasonMetadataService seasonMetadataService) {
         this.standingsController = standingsController;
         this.seasonMetadataService = seasonMetadataService;
+    }
+
+    @ModelAttribute("season_years")
+    public List<Integer> seasonYears() {
+        return seasonMetadataService.getAllSeasonYears();
     }
 
     @GetMapping(path = "")
