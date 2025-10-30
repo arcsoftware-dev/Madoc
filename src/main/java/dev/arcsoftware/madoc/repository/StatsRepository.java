48ff12cd361d64df2443c1757138d38d86b3cc62
Jake <arcdev.tech@gmail.com>
2025-10-30 12:41:59 -0400

    Goalie stats and fix filtering stats by year and season type

--- src/main/java/dev/arcsoftware/madoc/repository/StatsRepository.java
+++ src/main/java/dev/arcsoftware/madoc/repository/StatsRepository.java
@@ -118,6 +118,8 @@ public class StatsRepository {
         }
         return jdbcClient
                 .sql(StatsSql.GET_PLAYER_SEASON_TYPE_STATS_BY_YEAR)
+                .param("year", year)
+                .param("season_type", seasonType.name())
                 .query((rs, num) -> StatsDto.builder()
                         .playerName(rs.getString("player_name"))
                         .teamName(rs.getString("team_name"))
@@ -138,7 +140,22 @@ public class StatsRepository {
             }
             return staticGoalieStats2024;
         }
-        return Collections.emptyList();
+        return jdbcClient
+                .sql(StatsSql.GET_GOALIE_SEASON_TYPE_STATS_BY_YEAR)
+                .param("year", year)
+                .param("season_type", seasonType.name())
+                .query((rs, num) -> StatsDto.builder()
+                        .playerName(rs.getString("player_name"))
+                        .teamName(rs.getString("team_name"))
+                        .gamesPlayed(rs.getInt("games_played"))
+                        .wins(rs.getInt("wins"))
+                        .losses(rs.getInt("losses"))
+                        .ties(rs.getInt("ties"))
+                        .shutouts(rs.getInt("ties"))
+                        .penaltyMinutes(rs.getInt("penalty_minutes"))
+                        .goalsAgainst(rs.getInt("goals_against"))
+                        .build())
+                .list();
     }
 
     public void insertGoalAndAssists(GoalEntity goal) {
@@ -189,25 +206,37 @@ public class StatsRepository {
         WITH attendance AS (
             SELECT ra.id, count(*) AS games_played
             FROM madoc.attendance at
-                     JOIN madoc.roster_assignments ra ON at.roster_assignment_id = ra.id
+                JOIN madoc.roster_assignments ra ON at.roster_assignment_id = ra.id
+                JOIN madoc.games gm ON at.game_id = gm.id
+            WHERE gm.year = :year
+                AND gm.season_type = :season_type
             GROUP BY ra.id, at.attended
             HAVING at.attended = true
         ),
         goals AS (
             SELECT ra.id, count(*) AS goals_scored
             FROM madoc.goals go
-                     JOIN madoc.roster_assignments ra ON go.scorer_roster_assignment_id = ra.id
+                JOIN madoc.roster_assignments ra ON go.scorer_roster_assignment_id = ra.id
+                JOIN madoc.games gm ON go.game_id = gm.id
+            WHERE gm.year = :year
+                AND gm.season_type = :season_type
             GROUP BY ra.id
         ),
         assists AS (
             SELECT ra.id, count(*) AS total_assists
             FROM madoc.goals go
-                     JOIN madoc.roster_assignments ra ON go.primary_assist_roster_assignment_id = ra.id OR go.secondary_assist_roster_assignment_id = ra.id
+                JOIN madoc.roster_assignments ra ON go.primary_assist_roster_assignment_id = ra.id OR go.secondary_assist_roster_assignment_id = ra.id
+                JOIN madoc.games gm ON go.game_id = gm.id
+            WHERE gm.year = :year
+                AND gm.season_type = :season_type
             GROUP BY ra.id
         ),
         penalty_minutes AS (
             SELECT roster_assignment_id, SUM(minutes) AS total_penalty_minutes
-            FROM madoc.penalties
+            FROM madoc.penalties p
+            JOIN madoc.games gm ON p.game_id = gm.id
+            WHERE gm.year = :year
+                AND gm.season_type = :season_type
             GROUP BY roster_assignment_id
         )
         SELECT p.id AS player_id,
@@ -235,6 +264,82 @@ public class StatsRepository {
         public static final String CLEAR_PENALTIES_BY_GAME_ID = """
         DELETE FROM madoc.penalties
         WHERE game_id = :game_id
+        """;
+
+        public static final String GET_GOALIE_SEASON_TYPE_STATS_BY_YEAR = """
+        WITH attendance AS (
+            SELECT ra.id, count(*) AS games_played
+            FROM madoc.attendance at
+                JOIN madoc.roster_assignments ra ON at.roster_assignment_id = ra.id
+                JOIN madoc.games gm ON at.game_id = gm.id
+            WHERE gm.year = :year
+                AND gm.season_type = :season_type
+            GROUP BY ra.id, at.attended
+            HAVING at.attended = true
+        ),
+        penalty_minutes AS (
+             SELECT roster_assignment_id, SUM(minutes) AS total_penalty_minutes
+             FROM madoc.penalties
+             GROUP BY roster_assignment_id
+        ),
+        game_scores AS (
+            SELECT
+                gm.id AS game_id,
+                gm.home_team,
+                gm.away_team,
+                SUM(CASE WHEN ra.team_id = gm.home_team THEN 1 ELSE 0 END) AS home_score,
+                SUM(CASE WHEN ra.team_id = gm.away_team THEN 1 ELSE 0 END) AS away_score
+            FROM madoc.games gm
+                     LEFT JOIN madoc.goals g ON g.game_id = gm.id
+                     LEFT JOIN madoc.roster_assignments ra ON g.scorer_roster_assignment_id = ra.id AND ra.season_year = :year
+            WHERE gm.is_finalized = true
+              AND gm.season_type = :season_type
+              AND gm.year = :year
+            GROUP BY gm.id, gm.home_team, gm.away_team
+        ),
+        team_standings AS (
+            SELECT
+                t.id as team_id,
+                t.team_name,
+                COUNT(DISTINCT gs.game_id) AS games_played,
+                SUM(CASE
+                        WHEN (t.id = gs.home_team AND gs.home_score > gs.away_score)
+                            OR (t.id = gs.away_team AND gs.away_score > gs.home_score)
+                            THEN 1 ELSE 0 END) AS wins,
+                SUM(CASE
+                        WHEN (t.id = gs.home_team AND gs.home_score < gs.away_score)
+                            OR (t.id = gs.away_team AND gs.away_score < gs.home_score)
+                            THEN 1 ELSE 0 END) AS losses,
+                SUM(CASE WHEN gs.home_score = gs.away_score THEN 1 ELSE 0 END) AS ties,
+                SUM(CASE
+                        WHEN (t.id = gs.home_team AND gs.away_score = 0)
+                            OR (t.id = gs.away_team AND gs.home_score = 0)
+                            THEN 1 ELSE 0 END) AS shutouts,
+                SUM(CASE WHEN t.id = gs.home_team THEN gs.away_score
+                         WHEN t.id = gs.away_team THEN gs.home_score
+                         ELSE 0 END) AS goals_against
+            FROM madoc.teams t
+                     LEFT JOIN game_scores gs ON t.id = gs.home_team OR t.id = gs.away_team
+            GROUP BY t.id, t.team_name
+        )
+        SELECT p.id AS player_id,
+               ra.jersey_number AS jersey_number,
+               CONCAT(p.first_name, ' ', p.last_name) AS player_name,
+               t.team_name AS team_name,
+               COALESCE(attendance.games_played, 0) AS games_played,
+               ts.wins AS wins,
+               ts.losses  AS losses,
+               ts.ties  AS ties,
+               ts.shutouts  AS shutouts,
+               COALESCE(penalty_minutes.total_penalty_minutes, 0) AS penalty_minutes,
+               ts.goals_against  AS goals_against
+        FROM madoc.roster_assignments ra
+                 LEFT JOIN madoc.players p ON ra.player_id = p.id
+                 LEFT JOIN madoc.teams t ON ra.team_id = t.id
+                 LEFT JOIN attendance ON ra.id = attendance.id
+                 LEFT JOIN penalty_minutes ON ra.id = penalty_minutes.roster_assignment_id
+                 LEFT JOIN team_standings ts ON ra.team_id = ts.team_id
+        WHERE ra.position = 'GOALIE';
         """;
     }
 }
