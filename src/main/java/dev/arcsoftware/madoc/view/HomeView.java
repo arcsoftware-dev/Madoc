17b192ffd44c1ccff9be118268ee2b042e90b33a
Jake <arcdev.tech@gmail.com>
2026-01-02 10:51:37 -0500

    Fragment for banner and auto year on copyright footer

--- src/main/java/dev/arcsoftware/madoc/view/HomeView.java
+++ src/main/java/dev/arcsoftware/madoc/view/HomeView.java
@@ -10,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -34,6 +31,11 @@ public class HomeView {
         this.authorizationController = authorizationController;
         this.newsController = newsController;
         this.scheduleController = scheduleController;
+    }
+
+    @ModelAttribute("enableChristmasRaffleBanner")
+    public Boolean enableChristmasRaffleBanner() {
+        return false;
     }
 
     @GetMapping("/")
