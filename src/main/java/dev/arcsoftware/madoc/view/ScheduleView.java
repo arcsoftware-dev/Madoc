58191130bfe165e346324320060a5e5f84493c6d
Jake <arcdev.tech@gmail.com>
2026-02-16 13:45:01 -0500

    Add 2025 playoff schedule, schedule page navigation

--- src/main/java/dev/arcsoftware/madoc/view/ScheduleView.java
+++ src/main/java/dev/arcsoftware/madoc/view/ScheduleView.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -33,6 +34,10 @@ public class ScheduleView {
         this.seasonMetadataService = seasonMetadataService;
     }
 
+    @ModelAttribute("season_years")
+    public List<Integer> seasonYears() {
+        return seasonMetadataService.getAllSeasonYears();
+    }
 
     @GetMapping("")
     public String schedule(@RequestParam(value = "year", required = false) Integer year,
